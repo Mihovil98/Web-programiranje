8f4a111bc1a93a9cb27aaa2cc510994d069229b1
Mihovil <mihovilkovacevic1998@gmail.com>
2022-04-11 20:17:52 +0200

    LV2 commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showDescription1(){
+    document.getElementById('description').innerHTML = "John description"
+}
+
+function showDescription2(){
+    document.getElementById('description').innerHTML = "Jane description"
+}
+
+function showDescription3(){
+    document.getElementById('description').innerHTML = "Jack description"
+}
